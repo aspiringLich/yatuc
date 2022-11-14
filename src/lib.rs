827d1ab5d494cd5957b23af7b3faa7e3f7874b73
AspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-11-14 20:09:15 +0000

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![feature(default_free_fn)]
+
+extern crate hashbrown;
+extern crate parking_lot;
+extern crate bitvec;
+
+pub mod prelude;
+
+
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
---
+++ src/prelude.rs
@@ -0,0 +1,3 @@
+pub use std::default::default;
+#[cfg(test)]
+pub use pretty_assertions::{assert_eq, assert_ne, assert_str_eq};
