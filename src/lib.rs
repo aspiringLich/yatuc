e0aa46842638956b47c2ed40903ee9ef56d4783e
aspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-11-14 18:44:55 -0500

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,17 @@
-#![feature(default_free_fn)]
-
-pub extern crate hashbrown;
-pub extern crate parking_lot;
-pub extern crate bitvec;
 pub extern crate anyhow;
-pub extern crate colorful;
-pub extern crate chrono;
 pub extern crate autodefault;
+pub extern crate bitvec;
+pub extern crate chrono;
+pub extern crate colorful;
+pub extern crate derivative;
+pub extern crate hashbrown;
 pub extern crate num_enum;
+pub extern crate parking_lot;
 pub extern crate pretty_assertions;
 pub extern crate tracing;
+pub extern crate wyz;
 
 pub mod prelude;
-
 
 // pub fn add(left: usize, right: usize) -> usize {
 //     left + right
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,9 +1,10 @@
-pub use std::default::default;
-
+pub use anyhow::{anyhow, bail, ensure, Result};
 #[cfg(test)]
 pub use pretty_assertions::{assert_eq, assert_ne, assert_str_eq};
-pub use anyhow::{Result, anyhow, bail, ensure};
 
-pub use hashbrown::{HashMap, HashSet};
 pub use bitvec::{bitarr, bitbox, bits, bitvec};
+pub use default::default;
 pub use derivative::Derivative;
+pub use hashbrown::{HashMap, HashSet};
+pub use num_enum::{FromPrimitive, IntoPrimitive};
+pub use wyz::fmt::{FmtDisplay, FmtList};
